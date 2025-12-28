164c81bc65a7ad0a55db7b35b2f6ccc99fd6cbea
sreevalli <valliwebworkz@gmail.com>
2025-12-28 22:25:42 +0530

    fix: vite config and pathing

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lensmatrix/', // <--- Add this! It MUST match your repo name
+  base: '/lensmatrix/', 
 })
