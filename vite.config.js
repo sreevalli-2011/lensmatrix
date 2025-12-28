a2d2ec0a47aa72d12d161b374beb2c63fe2c7967
sreevalli <valliwebworkz@gmail.com>
2025-12-28 20:05:16 +0530

    Update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line below. Replace 'lensmatrix' if your repo name is different.
+  base: '/lensmatrix/', 
 })
