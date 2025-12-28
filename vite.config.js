44e9996366dd0f0bb30c8b95fc6fc4f0f49a78f4
sreevalli <valliwebworkz@gmail.com>
2025-12-28 21:59:54 +0530

    fix: white screen issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add this line below. Replace 'lensmatrix' if your repo name is different.
-  base: '/lensmatrix/', 
+  base: '/lensmatrix/', // <--- Add this! It MUST match your repo name
 })
